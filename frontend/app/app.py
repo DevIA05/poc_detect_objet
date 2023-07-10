6e468a9994ddd8091d85b38644905da88003488a
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-07-10 09:36:24 +0400

    aff img prédite

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -3,6 +3,8 @@ from PIL import Image
 from roboflow import Roboflow
 import tempfile
 import os
+import io
+import numpy as np
 
 print(os.getcwd())
 # Initialiser Roboflow avec votre clé API
@@ -27,9 +29,10 @@ if uploaded_file is not None:
     if st.button("Prédire"):
         # Enregistrer l'image redimensionnée temporairement sur le disque
         with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_image:
-            resized_image.save(temp_image.name)
+            image.save(temp_image.name)
         # Effectuer la prédiction avec Roboflow
-            pred = model.predict(temp_image.name, confidence=40, overlap=30)
+        path_save = "prediction.jpg"
+        pred = model.predict(temp_image.name, confidence=40, overlap=30)
         # prediction = model.predict(temp_image.name, confidence=50, overlap=50).json()
         # Supprimer le fichier temporaire
         os.remove(temp_image.name)
@@ -37,7 +40,20 @@ if uploaded_file is not None:
         st.subheader("Résultats de prédiction")
         st.json(pred.json())
         # Affiché l'image prédite
+        # Créer une nouvelle image à partir du tableau Numpy
         # path_save = "prediction.jpg"
-        # converted_image = pred.convert("RGB")
-        # converted_image.save(path_save)
-        # st.image(path_save, caption="Image prédite", use_column_width=True)
+        pred.save(path_save)
+        #image = Image.open(path_save)
+        # image_array = np.array(image)
+        # output_image = Image.fromarray(image_array)
+
+        # # Convertir l'image en bytes
+        # output_bytes = io.BytesIO()
+        # output_image.save(output_bytes, format="JPEG")
+
+        # # Afficher l'image prédite
+        # st.subheader("Image prédite")
+        # st.image(output_bytes, caption="Image prédite", use_column_width=True)
+        # # path_save = "prediction.jpg"
+        # # pred.save(path_save)
+        st.image(path_save, caption="Image prédite", use_column_width=True)
